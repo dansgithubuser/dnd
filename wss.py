017fd7f4cc884e082f07806843e562bd678e5401
Dan <dansonlinepresence@gmail.com>
2016-11-28 01:10:25 -0500

    save and load

---
+++ wss.py
@@ -0,0 +1,23 @@
+from websocket_server import WebsocketServer
+import json
+
+def handle_message(client, server, message):
+	try: j=json.loads(message)
+	except Exception as e:
+		print(e)
+		return
+	j['result']={}
+	j['result']['success']=True
+	try:
+		if j['command']=='load':
+			with open(j['path'], 'r') as file: j['result']['contents']=file.read()
+		if j['command']=='save':
+			with open(j['path'], 'w') as file: file.write(j['contents'])
+	except Exception as e:
+		j['result']['success']=False
+		j['result']['error']=str(e)
+	server.send_message(client, json.dumps(j))
+
+server=WebsocketServer(9160, host='localhost')
+server.set_fn_message_received(handle_message)
+server.run_forever()
