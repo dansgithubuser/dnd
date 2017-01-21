a8e870bb535dc90c0ea4cccad5ef9f9006e69336
Dan <dansonlinepresence@gmail.com>
2017-01-20 22:49:40 -0500

    refactor rolling

--- dnd.py
+++ dnd.py
@@ -1,13 +1,18 @@
 try: input=raw_input
 except: pass
 
-def interact():
+def roll(request):
 	import random, re
+	match=re.match(r'([0-9]+)?d([0-9]+)([+-][0-9]+)?', request)
+	if match:
+		dice, sides, bonus=[int(match.groups()[i] or [1, None, 0][i]) for i in range(3)]
+		rolls=[random.randint(1, sides) for i in range(dice)]
+		return (sum(rolls)+bonus, rolls)
+	return None
+
+def interact():
 	while(True):
 		request=input()
 		if request=='q': break
-		match=re.match(r'([0-9]+)?d([0-9]+)([+-][0-9]+)?', request)
-		if match:
-			dice, sides, bonus=[int(match.groups()[i] or [1, None, 0][i]) for i in range(3)]
-			rolls=[random.randint(1, sides) for i in range(dice)]
-			print(sum(rolls)+bonus, rolls)
+		r=roll(request)
+		if r: print(r)
