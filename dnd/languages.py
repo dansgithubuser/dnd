64190e9138d1ed39d7969b13bbf3c96d4e564cf1
Dan <dansonlinepresence@gmail.com>
2017-03-19 22:29:06 -0400

    voices

--- dnd/languages.py
+++ dnd/languages.py
@@ -1,3 +1,5 @@
+import base
+
 class Traits:
 	def __init__(self, typical_speakers, script):
 		self.typical_speakers=typical_speakers
@@ -23,3 +25,35 @@ exotic={
 	'sylvan': Traits(['fey_creatures'], 'elvish'),
 	'undercommon': Traits(['underdark_traders'], 'elvish'),
 }
+
+def random_tone():
+	return base.pick([
+		'excitable', 'calm', 'cranky', 'condescending',
+	])
+
+def random_vocalization():
+	return base.pick([
+		'crusty', 'stage_whisper', 'wheezy',
+	])
+
+def random_misc():
+	result=[]
+	if base.maybe(4): result.append('high_pitch')
+	if base.maybe(2): result.append(base.pick(['verbose', 'unvocal']))
+	if base.maybe(4): result.append(base.pick(['softspoken', 'loud']))
+	if base.maybe(4): result.append(base.pick(['shy', 'curious']))
+	return result
+
+def random_common_accent():
+	return base.pick([
+		'lisp', 'russian', 'american', 'swedish', 'scottish', 'french', 'rich',
+		'nerd', 'pinky',
+	])
+
+def random_human_voice():
+	result=[]
+	if base.maybe(2): result.append(random_common_accent())
+	if base.maybe(8): result.append(random_vocalization())
+	if base.maybe(4): result.append(random_tone())
+	result.extend(random_misc())
+	return result
