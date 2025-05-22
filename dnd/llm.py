b275e722ebf4ec0cf7b0dcadde66bb43569545b2
Dan <dansonlinepresence@gmail.com>
2025-05-22 19:06:44 -0400

    llm logging

--- dnd/llm.py
+++ dnd/llm.py
@@ -1,12 +1,15 @@
 from gpt4all import GPT4All
 
+from datetime import datetime
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
 def create_npc(system_message):
+    _tprint('creating NPC...')
     model = GPT4All('gpt4all-falcon-newbpe-q4_0.gguf')
     context = model.chat_session(system_message)
     context.__enter__()
+    _tprint('done!')
     return model, context
 
 def serve(model, password=None, port=8000):
@@ -17,9 +20,15 @@ def serve(model, password=None, port=8000):
                     return
             content_length = int(self.headers['Content-Length'])
             question = self.rfile.read(content_length).decode()
+            _tprint('<<<', question)
             answer = model.generate(question)
+            _tprint('>>>', answer)
             self.send_response(200)
             self.end_headers()
             self.wfile.write(answer.encode())
 
+    _tprint('serving...')
     HTTPServer(('0.0.0.0', port), Handler).serve_forever()
+
+def _tprint(*args, **kwargs):
+    print(datetime.now().astimezone().isoformat(' ', 'seconds'), *args, **kwargs)
